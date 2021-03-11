72b87e8b1191d2c02b33e2cfb2b2c8e8c359b29b
Robert Haines <robert.haines@anvildev.com>
2021-03-11 13:19:21 -0600

    Second button for hide areas added

---
+++ Scripts/PriceSetter.js
@@ -0,0 +1,12 @@
+﻿function SetPrice() {
+    var min = 1500;
+    var price = 315;
+    var sqft = document.getElementById("squareFeet").value;
+
+    while (sqft > min) {
+        price += 15;
+        min += 500;
+    }
+
+    document.getElementById("price").value = price;
+};
---
+++ Scripts/RealtorSelectorSwitch.js
@@ -0,0 +1,16 @@
+﻿
+function CloseNewRealtor() {
+    var check = document.getElementById("newBtn").getAttribute("aria-expanded");
+
+    if (check == "true") {
+        document.getElementById("newBtn").click();
+    }
+};
+
+function CloseExistingRealtor() {
+    var check = document.getElementById("existBtn").getAttribute("aria-expanded");
+
+    if (check == "true") {
+        document.getElementById("existBtn").click();
+    }
+};
